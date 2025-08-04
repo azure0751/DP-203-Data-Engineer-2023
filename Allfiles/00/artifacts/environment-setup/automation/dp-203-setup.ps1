ab79bdc9eb5d5bec53197b4d7a9e43de542f76b5
Abhi0751 <58366593+azure0751@users.noreply.github.com>
2025-08-04 17:09:22 +0530

    Update dp-203-setup.ps1

--- Allfiles/00/artifacts/environment-setup/automation/dp-203-setup.ps1
+++ Allfiles/00/artifacts/environment-setup/automation/dp-203-setup.ps1
@@ -150,7 +150,8 @@ while ($success -ne 1){
       $tried_list.Add($random_location)
       $locations = $locations | Where-Object {$_.Location -notin $tried_list}
       $rand = (0..$($locations.Count - 1)) | Get-Random
-      $random_location = $locations.Get($rand).Location
+      #$random_location = $locations.Get($rand).Location
+      $random_location ="centralus"  
     }
 }
 Remove-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $testServer | Out-Null
